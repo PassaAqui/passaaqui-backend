3d95140c8b764ace7e09ecd12c1cfe6da70201c6
harisoncleytondev <harisonc081@gmail.com>
2026-04-03 23:02:58 -0300

    chore: cria base central do projeto

---
+++ src/test/java/br/com/recifego/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.recifego.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
