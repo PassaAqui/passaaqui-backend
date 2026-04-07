c3601fa0338c989e93e433e1406ff6af1b9a6df0
harisoncleytondev <harisonc081@gmail.com>
2026-04-07 19:21:22 -0300

    feat: salva tokens (access e refresh) nos cookies.

--- src/main/java/br/com/recifego/api/modules/auth/controller/AuthController.java
+++ src/main/java/br/com/recifego/api/modules/auth/controller/AuthController.java
@@ -15,7 +15,9 @@ import br.com.recifego.api.modules.auth.service.AuthService;
 import br.com.recifego.api.modules.shopkeeper.model.ShopkeeperModel;
 import br.com.recifego.api.modules.tourist.model.TouristModel;
 import br.com.recifego.api.shared.objects.JWTObject;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -41,26 +43,61 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<JWTObject> login(@RequestBody @Valid LoginDTO dto, HttpServletRequest request) {
+    public ResponseEntity<JWTObject> login(@RequestBody @Valid LoginDTO dto, HttpServletRequest request, HttpServletResponse response) {
         String userAgent = request.getHeader("User-Agent");
         String ipAddress = request.getRemoteAddr();
         
         JWTObject tokens = service.loginAccount(dto.email(), dto.password(), userAgent, ipAddress);
+        setTokensInCookies(tokens, response);
         return ResponseEntity.ok(tokens);
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<JWTObject> refresh(@RequestBody @Valid RefreshTokenDTO dto, HttpServletRequest request) {
+    public ResponseEntity<JWTObject> refresh(@RequestBody @Valid RefreshTokenDTO dto, HttpServletRequest request, HttpServletResponse response) {
         String userAgent = request.getHeader("User-Agent");
         String ipAddress = request.getRemoteAddr();
         
         JWTObject tokens = service.refreshToken(dto.refreshToken(), userAgent, ipAddress);
+        setTokensInCookies(tokens, response);
         return ResponseEntity.ok(tokens);
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<Void> logout(@RequestBody @Valid RefreshTokenDTO dto) {
+    public ResponseEntity<Void> logout(@RequestBody @Valid RefreshTokenDTO dto, HttpServletResponse response) {
         service.logout(dto.refreshToken());
+        clearTokensFromCookies(response);
         return ResponseEntity.noContent().build();
+    }
+
+    private void setTokensInCookies(JWTObject tokens, HttpServletResponse response) {
+        Cookie accessTokenCookie = new Cookie("access_token", tokens.getAccess_token());
+        accessTokenCookie.setHttpOnly(true);
+        accessTokenCookie.setSecure(false); // deixar true, caso va pra prod.
+        accessTokenCookie.setPath("/");
+        accessTokenCookie.setMaxAge(900); 
+
+        Cookie refreshTokenCookie = new Cookie("refresh_token", tokens.getRefresh_token());
+        refreshTokenCookie.setHttpOnly(true);
+        refreshTokenCookie.setSecure(false); // deixar true, caso va pra prod.
+        refreshTokenCookie.setPath("/");
+        refreshTokenCookie.setMaxAge(604800); 
+
+        response.addCookie(accessTokenCookie);
+        response.addCookie(refreshTokenCookie);
+    }
+
+    private void clearTokensFromCookies(HttpServletResponse response) {
+        Cookie accessTokenCookie = new Cookie("access_token", null);
+        accessTokenCookie.setHttpOnly(true);
+        accessTokenCookie.setPath("/");
+        accessTokenCookie.setMaxAge(0);
+
+        Cookie refreshTokenCookie = new Cookie("refresh_token", null);
+        refreshTokenCookie.setHttpOnly(true);
+        refreshTokenCookie.setPath("/");
+        refreshTokenCookie.setMaxAge(0);
+
+        response.addCookie(accessTokenCookie);
+        response.addCookie(refreshTokenCookie);
     }
 }
