0a700682498b8768d2538e40dfbf8e84fb325314
harisoncleytondev <harisonc081@gmail.com>
2026-07-26 18:50:29 -0300

    feat: adiciona POI associado na resposta do /shopkeepers/me

--- src/main/java/com/passaaqui/backend/modules/poi/repository/PoiRepository.java
+++ src/main/java/com/passaaqui/backend/modules/poi/repository/PoiRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PoiRepository extends JpaRepository<PoiModel, Integer> {
+    Optional<PoiModel> findByShopkeeperId(Integer shopkeeperId);
 
     @Query("SELECT p FROM PoiModel p WHERE p.latitude IS NOT NULL AND p.longitude IS NOT NULL " +
            "AND p.latitude BETWEEN :minLat AND :maxLat AND p.longitude BETWEEN :minLon AND :maxLon")
